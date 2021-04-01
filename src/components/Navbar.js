57cc141d87b246fb004bcd1f3aa959d4c6148d4d
Enveryakubov <enveryakubov@gmail.com>
2021-04-01 14:38:46 +0300

    Added burger icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 
 import {FiBookOpen} from "react-icons/fi"
+import {GiHamburgerMenu} from "react-icons/gi"
 
 const Navbar = ({setActivity}) => {
   
@@ -10,9 +11,12 @@ const Navbar = ({setActivity}) => {
             <div className="container-fluid p-0">
                 <div className="navbar-brand pt-0" style={{ fontSize:"40px"}}>
                     <div onClick={() => setActivity("home")} style={{display:"flex", justifyContent:"space-between"}}>
-                   <FiBookOpen />
-                   <h4 style={{paddingLeft:"10px", paddingTop:"5px"}}>Чтение</h4>
+                        <FiBookOpen />
+                        <h4 style={{paddingLeft:"10px", paddingTop:"5px"}}>Чтение</h4>
                    </div>
+                </div>
+                <div onClick={() => setActivity("home")}>
+                    <GiHamburgerMenu style={{fontSize:"40px", marginRight:"20px"}}/>
                 </div>
             </div>
         </nav>
